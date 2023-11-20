fd70c82fc8ea2aee7d1dcf9525f061ced2295843
willemverspyck <willemverspyck@users.noreply.github.com>
2023-11-20 11:23:59 +0100

    Default add response group

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Spyck\ApiExtension\Controller;
 
+use Spyck\ApiExtension\Model\Response as ResponseAsModel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as BaseAbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +14,8 @@ abstract class AbstractController extends BaseAbstractController
 {
     protected function getResponseForItem(object $data = null, array $groups = []): JsonResponse
     {
+        $groups[] = ResponseAsModel::GROUP;
+
         if (null === $data) {
             $error = [
                 'Item not found',
@@ -30,6 +33,8 @@ abstract class AbstractController extends BaseAbstractController
 
     protected function getResponseForList(object $data, array $groups = []): JsonResponse
     {
+        $groups[] = ResponseAsModel::GROUP;
+
         return $this->json($data, Response::HTTP_OK, [], [
             AbstractNormalizer::GROUPS => $groups,
         ]);
