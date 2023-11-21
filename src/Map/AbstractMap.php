3619f50baaec734e2cc37bbf299c8ff0289434e8
Willem Verspyck <willemverspyck@users.noreply.github.com>
2023-11-21 21:21:54 +0100

    CI fix

--- src/Map/AbstractMap.php
+++ src/Map/AbstractMap.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Spyck\ApiExtension\Map;
 
-use OpenApi\Attributes as OpenApi;
-use Symfony\Component\Validator\Constraints as Validator;
-
 abstract class AbstractMap implements MapInterface
 {
 }
