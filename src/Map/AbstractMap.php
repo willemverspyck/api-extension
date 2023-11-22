b1b72d5bf067085eb3f6c2e30462ea80661af738
willemverspyck <willemverspyck@users.noreply.github.com>
2023-11-22 12:13:34 +0100

    Change directory and fix namespace

--- src/Map/AbstractMap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Spyck\ApiExtension\Map;
-
-abstract class AbstractMap implements MapInterface
-{
-}
--- src/Schema/Request.php
+++ src/Schema/Request.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Schema;
+namespace Spyck\ApiExtension\Schema;
 
 use Attribute;
 use Nelmio\ApiDocBundle\Annotation\Model;
