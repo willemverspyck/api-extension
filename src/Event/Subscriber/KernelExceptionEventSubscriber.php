5d24e9d5eff0d7bd43500698803bb4570bf3ef15
willemverspyck <willemverspyck@users.noreply.github.com>
2026-06-16 17:08:49 +0200

    Add strict types

--- src/Event/Subscriber/KernelExceptionEventSubscriber.php
+++ src/Event/Subscriber/KernelExceptionEventSubscriber.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Spyck\ApiExtension\Event\Subscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
--- src/Map/Timestamp.php
+++ src/Map/Timestamp.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Spyck\ApiExtension\Map;
 
 use DateTimeImmutable;
